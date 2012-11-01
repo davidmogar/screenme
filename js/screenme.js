84bf3ce33eaf48d5072e786093267b48783ab583
David Moreno García <david.mogar@gmail.com>
2012-11-01 03:59:12 +0100

    Added initial support for Slideshare.

--- js/screenme.js
+++ js/screenme.js
@@ -5,12 +5,37 @@
  */
 
 $(document).ready(function () {
-  $("#webcam").resizable({
+  $("#slides_pane").resizable({
+    containment: "#wrapper",
+    grid: 1,
+    start: function(){
+      iframe = $('iframe');
+      var div = $('<div></div>');
+
+      $('#slides_pane').append(div[0]);
+      div[0].id = 'temp_div';
+      div.css({position:'absolute'});
+      div.css({top: iframe.position().top, left:0});
+      div.height(iframe.height());
+      div.width('100%');
+    },
+    stop: function(){
+      $('#temp_div').remove();
+    }
+  });
+  $("#slides_pane").draggable({
+    containment: "#wrapper",
+    scroll: false,
+    snap: "#wrapper",
+    snapMode: "inner"
+  });
+
+  $("#webcam_pane").resizable({
     aspectRatio: 4/3,
     containment: "#wrapper",
     grid: 1
   });
-  $("#webcam").draggable({
+  $("#webcam_pane").draggable({
     containment: "#wrapper",
     scroll: false,
     snap: "#wrapper",
@@ -18,6 +43,7 @@ $(document).ready(function () {
   });
 
   loadUserMedia();
+  loadSlideshare('7292569');
 });
 
 var onUserMediaError = function(e) {
@@ -28,11 +54,18 @@ function loadUserMedia() {
   if (navigator.getUserMedia) {                       
     navigator.getUserMedia({video: true, audio: true},
         function(localMediaStream) {
-      var video = $("#webcam video");
+      var video = $("#webcam");
       video.attr('src', window.URL.createObjectURL(localMediaStream));
     }, onUserMediaError); 
   } else {                                                                                                 alert('Your browser is not supported.'); 
   }
+}
+
+function loadSlideshare(embedCode) {
+  var src = 'http://www.slideshare.net/slideshow/embed_code/' + embedCode; 
+  $('#slides_pane').append(
+    '<iframe src="' + src + '""></iframe>'
+  );
 }
 
 window.URL = window.URL || window.webkitURL;
